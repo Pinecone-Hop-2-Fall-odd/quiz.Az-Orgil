3de0d54e482e4fb5a7e3a3606ddbedc051e96de8
Az-Orgil <azorgilnyamdorj@gmail.com>
2023-12-20 19:34:22 +0800

    add

--- quiz/src/app/page.js
+++ quiz/src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   const router = useRouter();
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = sessionStorage.getItem('token');
     if(!token) {
       router.push('/login')
     }
